d06f404e14f298f542000ee8e7896c1214f4d53b
vinayrewatkar <vinayrewatkar.257@gmail.com>
2024-12-31 16:24:44 +0530

    resolve mongodb connect error

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,20 +1,20 @@
 const mongoose = require('mongoose');
 
-const connectToMongoDB = () => {
-    const uri = process.env.MONGO_URI; 
+const connectToMongoDB = async () => {
+    const uri = process.env.MONGO_URI;
 
     if (!uri) {
         console.error('MONGO_URI is not defined in the environment variables');
-        process.exit(1); 
+        process.exit(1);
     }
 
-    mongoose
-        .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
-        .then(() => console.log('MongoDB connection successful'))
-        .catch(err => {
-            console.error('MongoDB connection error:', err);
-            process.exit(1); 
-        });
+    try {
+        await mongoose.connect(uri);
+        console.log('MongoDB connection successful');
+    } catch (err) {
+        console.error('MongoDB connection error:', err);
+        process.exit(1);
+    }
 };
 
 module.exports = { connectToMongoDB };
